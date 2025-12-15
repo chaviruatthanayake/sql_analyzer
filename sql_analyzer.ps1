7b6b5c5dc4cfe63a1f649180351827e82e9a7bfd
chaviruatthanayake <chaviruatthanayake@gmail.com>
2025-12-15 21:29:23 +0530

    Added sys.master_files details to output

--- sql_analyzer.ps1
+++ sql_analyzer.ps1
@@ -5,7 +5,7 @@ Add-Type -AssemblyName System.Drawing
 # Create the form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "SQL Server Configuration Analyzer"
-$form.Size = New-Object System.Drawing.Size(600,400)
+$form.Size = New-Object System.Drawing.Size(900,600)
 $form.StartPosition = "CenterScreen"
 
 # Server name label and textbox
@@ -17,13 +17,13 @@ $form.Controls.Add($labelServer)
 
 $textBoxServer = New-Object System.Windows.Forms.TextBox
 $textBoxServer.Location = New-Object System.Drawing.Point(150,20)
-$textBoxServer.Size = New-Object System.Drawing.Size(400,20)
+$textBoxServer.Size = New-Object System.Drawing.Size(700,20)
 $form.Controls.Add($textBoxServer)
 
 # RichTextBox for colorful output
 $richTextBoxOutput = New-Object System.Windows.Forms.RichTextBox
 $richTextBoxOutput.Location = New-Object System.Drawing.Point(20,60)
-$richTextBoxOutput.Size = New-Object System.Drawing.Size(530,250)
+$richTextBoxOutput.Size = New-Object System.Drawing.Size(840,450)
 $richTextBoxOutput.ReadOnly = $true
 $richTextBoxOutput.BackColor = [System.Drawing.Color]::White
 $form.Controls.Add($richTextBoxOutput)
@@ -31,7 +31,7 @@ $form.Controls.Add($richTextBoxOutput)
 # Button to run analysis
 $buttonRun = New-Object System.Windows.Forms.Button
 $buttonRun.Text = "Run Analysis"
-$buttonRun.Location = New-Object System.Drawing.Point(20,320)
+$buttonRun.Location = New-Object System.Drawing.Point(20,520)
 $buttonRun.Size = New-Object System.Drawing.Size(100,30)
 $form.Controls.Add($buttonRun)
 
@@ -67,16 +67,12 @@ $buttonRun.Add_Click({
         [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
         $server = New-Object Microsoft.SqlServer.Management.Smo.Server $serverName
         
-        # Set connection properties to avoid enumeration issues
+        # Set connection properties
         $server.ConnectionContext.ConnectTimeout = 30
-        $server.ConnectionContext.StatementTimeout = 30
+        $server.ConnectionContext.StatementTimeout = 60
         
-        # Set default init fields to minimize property fetching
-        $server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database], $false)
-        $server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Table], $false)
-        $server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.StoredProcedure], $false)
-        $server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.View], $false)
-        $server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.UserDefinedFunction], $false)
+        # Force connection
+        $null = $server.Version
         
         $richTextBoxOutput.Clear()
         Append-ColoredText $richTextBoxOutput "SQL Server Analysis for: $serverName" ([System.Drawing.Color]::Blue) $true
@@ -93,7 +89,7 @@ $buttonRun.Add_Click({
         # System databases to exclude
         $systemDatabases = @('master', 'model', 'msdb', 'tempdb')
         
-        # Get list of user databases using SQL query instead of SMO enumeration
+        # Get list of user databases using SQL query
         $userDatabaseNames = @()
         try {
             $query = "SELECT name FROM sys.databases WHERE name NOT IN ('master', 'model', 'msdb', 'tempdb') ORDER BY name"
@@ -102,8 +98,7 @@ $buttonRun.Add_Click({
                 $userDatabaseNames += $row["name"]
             }
         } catch {
-            # Fallback to SMO if query fails
-            $userDatabaseNames = $server.Databases | Where-Object { $systemDatabases -notcontains $_.Name } | Select-Object -ExpandProperty Name
+            Append-ColoredText $richTextBoxOutput "`r`nError getting database list: $($_.Exception.Message)" ([System.Drawing.Color]::Red) $false
         }
         
         # Get total database count using SQL query
@@ -123,25 +118,29 @@ $buttonRun.Add_Click({
             try {
                 Append-ColoredText $richTextBoxOutput "`r`nDatabase: $dbName" ([System.Drawing.Color]::DarkRed) $true
                 
-                # Use dynamic SQL with sp_executesql to properly query each database
-                
                 # Get database size
                 try {
-                    $sizeQuery = "EXEC [$dbName]..sp_executesql N'SELECT SUM(CAST(size AS BIGINT) * 8 / 1024.0) AS SizeMB FROM sys.database_files'"
+                    $sizeQuery = @"
+USE [$dbName];
+SELECT SUM(CAST(size AS BIGINT) * 8 / 1024.0) AS SizeMB FROM sys.database_files;
+"@
                     $sizeResult = $server.ConnectionContext.ExecuteWithResults($sizeQuery)
-                    if ($sizeResult.Tables[0].Rows.Count -gt 0) {
+                    if ($sizeResult.Tables[0].Rows.Count -gt 0 -and $null -ne $sizeResult.Tables[0].Rows[0]["SizeMB"]) {
                         $sizeMB = [math]::Round($sizeResult.Tables[0].Rows[0]["SizeMB"], 2)
                         Append-ColoredText $richTextBoxOutput "  Size: $sizeMB MB" ([System.Drawing.Color]::DarkGreen) $true
                     } else {
                         Append-ColoredText $richTextBoxOutput "  Size: 0 MB" ([System.Drawing.Color]::DarkGreen) $true
                     }
                 } catch {
-                    Append-ColoredText $richTextBoxOutput "  Size: Unable to retrieve ($($_.Exception.Message))" ([System.Drawing.Color]::DarkGreen) $true
+                    Append-ColoredText $richTextBoxOutput "  Size: Unable to retrieve" ([System.Drawing.Color]::DarkGreen) $true
                 }
                 
                 # Get data file count
                 try {
-                    $fileQuery = "EXEC [$dbName]..sp_executesql N'SELECT COUNT(*) AS FileCount FROM sys.database_files WHERE type = 0'"
+                    $fileQuery = @"
+USE [$dbName];
+SELECT COUNT(*) AS FileCount FROM sys.database_files WHERE type = 0;
+"@
                     $fileResult = $server.ConnectionContext.ExecuteWithResults($fileQuery)
                     $fileCount = $fileResult.Tables[0].Rows[0]["FileCount"]
                     Append-ColoredText $richTextBoxOutput "  Data Files: $fileCount" ([System.Drawing.Color]::DarkBlue) $true
@@ -149,9 +148,12 @@ $buttonRun.Add_Click({
                     Append-ColoredText $richTextBoxOutput "  Data Files: Unable to retrieve" ([System.Drawing.Color]::DarkBlue) $true
                 }
                 
-                # Get stored procedure count (user objects only)
+                # Get stored procedure count
                 try {
-                    $spQuery = "EXEC [$dbName]..sp_executesql N'SELECT COUNT(*) AS SPCount FROM sys.procedures WHERE is_ms_shipped = 0'"
+                    $spQuery = @"
+USE [$dbName];
+SELECT COUNT(*) AS SPCount FROM sys.procedures WHERE is_ms_shipped = 0;
+"@
                     $spResult = $server.ConnectionContext.ExecuteWithResults($spQuery)
                     $spCount = $spResult.Tables[0].Rows[0]["SPCount"]
                     Append-ColoredText $richTextBoxOutput "  User Stored Procedures: $spCount" ([System.Drawing.Color]::DarkMagenta) $true
@@ -159,9 +161,12 @@ $buttonRun.Add_Click({
                     Append-ColoredText $richTextBoxOutput "  User Stored Procedures: Unable to retrieve" ([System.Drawing.Color]::DarkMagenta) $true
                 }
                 
-                # Get view count (user objects only)
+                # Get view count
                 try {
-                    $viewQuery = "EXEC [$dbName]..sp_executesql N'SELECT COUNT(*) AS ViewCount FROM sys.views WHERE is_ms_shipped = 0'"
+                    $viewQuery = @"
+USE [$dbName];
+SELECT COUNT(*) AS ViewCount FROM sys.views WHERE is_ms_shipped = 0;
+"@
                     $viewResult = $server.ConnectionContext.ExecuteWithResults($viewQuery)
                     $viewCount = $viewResult.Tables[0].Rows[0]["ViewCount"]
                     Append-ColoredText $richTextBoxOutput "  User Views: $viewCount" ([System.Drawing.Color]::DarkCyan) $true
@@ -171,7 +176,10 @@ $buttonRun.Add_Click({
                 
                 # Get user defined function count
                 try {
-                    $funcQuery = "EXEC [$dbName]..sp_executesql N'SELECT COUNT(*) AS FuncCount FROM sys.objects WHERE type IN (''FN'', ''IF'', ''TF'') AND is_ms_shipped = 0'"
+                    $funcQuery = @"
+USE [$dbName];
+SELECT COUNT(*) AS FuncCount FROM sys.objects WHERE type IN ('FN', 'IF', 'TF') AND is_ms_shipped = 0;
+"@
                     $funcResult = $server.ConnectionContext.ExecuteWithResults($funcQuery)
                     $funcCount = $funcResult.Tables[0].Rows[0]["FuncCount"]
                     Append-ColoredText $richTextBoxOutput "  User Defined Functions: $funcCount" ([System.Drawing.Color]::Chocolate) $true
@@ -181,7 +189,10 @@ $buttonRun.Add_Click({
                 
                 # Get trigger count
                 try {
-                    $triggerQuery = "EXEC [$dbName]..sp_executesql N'SELECT COUNT(*) AS TriggerCount FROM sys.triggers WHERE parent_class = 1 AND is_ms_shipped = 0'"
+                    $triggerQuery = @"
+USE [$dbName];
+SELECT COUNT(*) AS TriggerCount FROM sys.triggers WHERE parent_class = 1 AND is_ms_shipped = 0;
+"@
                     $triggerResult = $server.ConnectionContext.ExecuteWithResults($triggerQuery)
                     $triggerCount = $triggerResult.Tables[0].Rows[0]["TriggerCount"]
                     Append-ColoredText $richTextBoxOutput "  Triggers: $triggerCount" ([System.Drawing.Color]::DarkGoldenrod) $true
@@ -194,9 +205,58 @@ $buttonRun.Add_Click({
             }
         }
         
+        Append-ColoredText $richTextBoxOutput "`r`nDatabase Files (sys.master_files):" ([System.Drawing.Color]::Purple) $true
+        try {
+            $masterFilesQuery = @"
+SELECT 
+    DB_NAME(database_id) AS DatabaseName,
+    name AS LogicalName,
+    physical_name AS PhysicalPath,
+    CASE type
+        WHEN 0 THEN 'Data'
+        WHEN 1 THEN 'Log'
+        WHEN 2 THEN 'FileStream'
+        WHEN 3 THEN 'FullText'
+        ELSE 'Unknown'
+    END AS FileType,
+    CAST(size * 8.0 / 1024 AS DECIMAL(10,2)) AS SizeMB,
+    CASE 
+        WHEN max_size = -1 THEN 'Unlimited'
+        WHEN max_size = 0 THEN '0 MB'
+        ELSE CAST(max_size * 8.0 / 1024 AS VARCHAR(20)) + ' MB'
+    END AS MaxSize,
+    CASE 
+        WHEN is_percent_growth = 1 THEN CAST(growth AS VARCHAR(10)) + '%'
+        ELSE CAST(growth * 8.0 / 1024 AS VARCHAR(10)) + ' MB'
+    END AS GrowthSetting,
+    state_desc AS State
+FROM sys.master_files
+WHERE DB_NAME(database_id) NOT IN ('master', 'model', 'msdb', 'tempdb')
+ORDER BY DatabaseName, FileType, name
+"@
+            $masterFilesResult = $server.ConnectionContext.ExecuteWithResults($masterFilesQuery)
+            
+            if ($masterFilesResult.Tables[0].Rows.Count -eq 0) {
+                Append-ColoredText $richTextBoxOutput "  No user database files found" ([System.Drawing.Color]::DarkSlateGray) $false
+            } else {
+                $currentDb = ""
+                foreach ($row in $masterFilesResult.Tables[0].Rows) {
+                    if ($currentDb -ne $row['DatabaseName']) {
+                        $currentDb = $row['DatabaseName']
+                        Append-ColoredText $richTextBoxOutput "`r`n  Database: $currentDb" ([System.Drawing.Color]::DarkRed) $true
+                    }
+                    $fileInfo = "    [$($row['FileType'])] $($row['LogicalName'])"
+                    Append-ColoredText $richTextBoxOutput $fileInfo ([System.Drawing.Color]::DarkBlue) $false
+                    Append-ColoredText $richTextBoxOutput "      Path: $($row['PhysicalPath'])" ([System.Drawing.Color]::DarkGreen) $false
+                    Append-ColoredText $richTextBoxOutput "      Size: $($row['SizeMB']) MB | Max: $($row['MaxSize']) | Growth: $($row['GrowthSetting']) | State: $($row['State'])" ([System.Drawing.Color]::DarkCyan) $false
+                }
+            }
+        } catch {
+            Append-ColoredText $richTextBoxOutput "  Unable to retrieve master_files info: $($_.Exception.Message)" ([System.Drawing.Color]::Red) $false
+        }
+        
         Append-ColoredText $richTextBoxOutput "`r`nLogins:" ([System.Drawing.Color]::Purple) $true
         try {
-            # Use fully qualified query - no need to switch databases
             $loginQuery = @"
 SELECT 
     name,
